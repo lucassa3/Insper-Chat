170d0f95fd01d31529c9e34de5aa0b61281cf378
lucassa3 <lucassa3@al.insper.edu.br>
2016-12-01 21:57:20 -0200

    implementing user differentiation

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public')); //precisei colocar os arquivos estaticos como um middleware antes do route handler
+
+app.get('/', function(req, res){  //route handler, faz get no html do chat
+  res.sendfile('chat_screen.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
