38ff52d452ce07d5ea74febc4b2d29fa8bfd9232
lucassa3 <lucassa3@al.insper.edu.br>
2016-12-01 23:17:50 -0200

    chat fully working

--- Controller.js
+++ Controller.js
@@ -3,31 +3,53 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/public')); //precisei colocar os arquivos estaticos como um middleware antes do route handler
+app.use(express.static(__dirname + '/View')); //precisei colocar os arquivos estaticos como um middleware antes do route handler
 
 app.get('/', function(req, res){  //route handler, faz get no html do chat
-  res.sendfile('chat_screen.html');
+  res.sendFile(__dirname + '/View/chat_screen.html');
 });
 
 io.on('connection', function(socket){
+
   console.log('a user connected');
+
   socket.on('disconnect', function(){
+
     console.log('user disconnected');
+
   });
 });
 
+
+
 io.on('connection', function(socket){
+
   socket.on('chat message', function(msg){
+
     console.log('message: ' + msg);
+
+    io.emit('chat message', msg);
+
   });
 });
+
+
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+
+  socket.on('name', function(msg){
+
+    console.log('name: ' + msg);
+
+    io.emit('name', msg);
+
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+
+
+http.listen(8080, function(){
+
+  console.log('conectando na porta 8080!');
+
 });
--- README.md
+++
@@ -1,2 +0,0 @@
-# Insper-Chat
-Sketch of a NodeJS-based real-time chat, where you can communicate with your local network friends.
